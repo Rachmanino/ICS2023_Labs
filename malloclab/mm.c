f83eaa80e35947e4980bb7506b115677b2b592a8
rachmanino <18805904201@163.com>
2023-12-11 01:43:42 +0800

    Class machine 92 stable final version.

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -2,14 +2,15 @@
  * mm.c
  *
  * Name: 吴童
- * Student ID: 2200013212// 
+ * Student ID: 2200013212
  *
  * Description: 我采用segregated fit + best fit 策略实现。维护11个链表存储块大小：
- * - Segregated fit: 维护11个链表存储块,大小：16~32, 33~64, 65~80, 81~96, 97~128, 
+ * - Segregated fit: 维护11个链表存储块,大小：16~32, 33~64, 65~80, 81~96, 97~128,
  *                   129~256, 257~512, 513~1024, 1025~2048, 2049~4096, 4097~inf
  *                   每个链表的头指针存储在heap_listp + i * WSIZE中，
- *                   链表中的每个块的前驱和后继指针存储在块指针处，头部尾部存放块大（我没有实现去脚部）   
- *                   trick: 我存储前驱后继指针时只存下了相对于heap_listp的偏移量，这样可以只各占4字节 
+ *                   链表中的每个块的前驱和后继指针存储在块指针处，头部尾部存放块大（我没有实现去脚部）
+ *                   trick: 我存储前驱后继指针时只存下了相对于heap_start的偏移量，这样可以只各占4字节
+ *                   (因此在整个程序中我判空都是与heap_start作比较，而非NULL)
  * - Best fit: 在每个链表中，从小到大遍历，找到第一个满足要求的块
  *             （我在空闲块插入时维护了每个链表内部的大小有序）
  * - 其他优化：1. 分割时，将大块和小块分开存放（受到默认malloc的启发，分开存放有助于free时合并）
@@ -26,8 +27,10 @@
 #include "memlib.h"
 
  /* If you want debugging output, use the following macro.  When you hand
-  * in, remove the #define DEBUG line. */
-// #define DEBUG
+  * in, remove the #define DEBUG line. */ 
+/* uncommment this line to enable debug mode */
+//#define DEBUG
+/* ------------------------------------------*/
 #ifdef DEBUG
 #define dbg_printf(...) printf(__VA_ARGS__)
 #else
@@ -80,7 +83,8 @@
 
 /* ################################ 以下为我需要的全局变量，函数和宏 #################################*/
 /* Global variables */
-static void* heap_listp = 0; /* Pointer to heap entry */
+static void* heap_listp = 0; /* Pointer to prelogue heap */
+static void* heap_start = 0; /* Pointer to heap start /
 
 /* Function prototypes for internal helper routines */
 static void* extend_heap(size_t words);
@@ -95,9 +99,9 @@ static void print_free_list();   // DEBUG用，打印空闲链表
 
 /* Extra macros */
 #define CLASS_NUM 11                                                                 // 空闲链表类数量
-#define GET_LINK_FIRST(index) (heap_listp + (long)(GET(heap_listp + index * WSIZE))) // 获取第index个空闲链表的头指针 index: 0~CLASS_NUM-1
-#define GET_LINK_PRED(bp) (heap_listp + (GET((char *)(bp))))                         // 获取位于bp的空闲块的链表前驱指针
-#define GET_LINK_SUCC(bp) (heap_listp + (GET((char *)(bp) + WSIZE)))                 // 获取位于bp的空闲块的链表后继指针
+#define GET_LINK_FIRST(index) (heap_start + (long)(GET(heap_start + index * WSIZE))) // 获取第index个空闲链表的头指针 index: 0~CLASS_NUM-1
+#define GET_LINK_PRED(bp) (heap_start + (GET((char *)(bp))))                         // 获取位于bp的空闲块的链表前驱指针
+#define GET_LINK_SUCC(bp) (heap_start + (GET((char *)(bp) + WSIZE)))                 // 获取位于bp的空闲块的链表后继指针
 /* ############################################################################################*/
 
 /*
@@ -119,6 +123,8 @@ int mm_init(void) {
     PUT(heap_listp + ((CLASS_NUM + 1) * WSIZE), PACK(DSIZE, 1)); /* Prologue footer */
     PUT(heap_listp + ((CLASS_NUM + 2) * WSIZE), PACK(0, 1));     /* Epilogue header */
 
+    heap_start = heap_listp;
+    heap_listp += (CLASS_NUM + 1) * WSIZE;
     /* Extend the empty heap with a free block of CHUNKSIZE bytes */
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
         return -1;
@@ -126,18 +132,19 @@ int mm_init(void) {
 }
 
 /*
- * malloc
+ * malloc : Search the free list for a fit, ask kernal for extra space otherwise
  */
 void* malloc(size_t size) {
 #ifdef DEBUG
     print_free_list();
     printf("malloc: %lu\n", size);
 #endif
+
     size_t asize;      /* Adjusted block size */
     size_t extendsize; /* Amount to extend heap if no fit */
     char* bp;
 
-    if (heap_listp == 0) {
+    if (heap_start == 0) {
         mm_init();
     }
     /* Ignore spurious requests */
@@ -153,7 +160,7 @@ void* malloc(size_t size) {
     /* Search the free list for a fit */
     if ((bp = find_fit(asize)) != NULL) {
 #ifdef DEBUG
-        printf("find fit at %llx\n", bp);
+        printf("find fit at %p\n", bp);
 #endif
         return place(bp, asize);
     }
@@ -167,7 +174,7 @@ void* malloc(size_t size) {
 }
 
 /*
- * free
+ * free: naive free
  */
 void free(void* ptr) {
     if (ptr == NULL)
@@ -186,7 +193,7 @@ void free(void* ptr) {
 }
 
 /*
- * realloc
+ * realloc: 这里对一种特殊情况做了判断
  */
 void* realloc(void* oldptr, size_t size) {
     size_t oldsize,
@@ -206,8 +213,8 @@ void* realloc(void* oldptr, size_t size) {
     }
 
     oldsize = GET_SIZE(HDRP(oldptr));
-    nextsize = GET_SIZE(HDRP(NEXT_BLKP(oldptr)));   
-    
+    nextsize = GET_SIZE(HDRP(NEXT_BLKP(oldptr)));
+
     /* 计算分配大小: +8然后向上按DSIZE倍数对齐 */
     if (size <= DSIZE)
         asize = 2 * DSIZE;
@@ -249,9 +256,78 @@ static int aligned(const void* p) {
 }
 
 /*
- * mm_checkheap
+ * mm_checkheap：check heap consistency, for DEBUG
  */
+ /* useful functions for checking blocks and lists */
+void check_block(void* bp) {
+    if (!in_heap(HDRP(bp)) || !in_heap(FTRP(bp)+WSIZE)) {
+        printf("Block at %p is out of heap!\n", bp);
+        exit(-1);
+    }
+    if (memcmp(HDRP(bp), FTRP(bp), WSIZE) != 0) {
+        printf("Block at %p: header and footer not equal!\n", bp);
+        exit(-1);
+    }
+    if (!aligned(bp) || GET_SIZE(HDRP(bp)) % DSIZE) {
+        printf("Block at %p is not aligned!\n", bp);
+        exit(-1);
+    }
+    if (GET_SIZE(HDRP(bp)) < 2 * DSIZE) {
+        printf("Block at %p is too small!\n", bp);
+        exit(-1);
+    }   
+}
 void mm_checkheap(int lineno) {
+    /* Check whether the heap is initialized */
+    if (heap_start != mem_heap_lo()) {
+        printf("Bad heap start: %p, should be: %p\n", heap_start, mem_heap_lo());
+        exit(-1);
+    }
+
+    /* Check free list */
+    int free_num_linked = 0;
+    for (int i = 0; i < CLASS_NUM; i++) {
+        char* cur = GET_LINK_FIRST(i);
+        if (cur == heap_start)
+            continue;
+        while (cur != heap_start) {
+            if (GET_LINK_SUCC(cur) != heap_start && GET_LINK_PRED(GET_LINK_SUCC(cur)) != cur) {
+                printf("Bad linked after: %p\n", cur);
+                exit(-1);
+            }
+            if (GET_LINK_PRED(cur) != heap_start && GET_LINK_SUCC(GET_LINK_PRED(cur)) != cur) {
+                printf("Bad linked before: %p\n", cur);
+                exit(-1);
+            }
+            free_num_linked++;
+            cur = GET_LINK_SUCC(cur);
+        }
+    }
+    
+    /* Check the whole heap */
+    int free_num_heap = 0;
+    char* cur = NEXT_BLKP(heap_listp);
+    while (in_heap(cur)) {
+        check_block(cur);
+        if (FTRP(cur) + WSIZE != HDRP(NEXT_BLKP(cur))) {
+            printf("Bad heap neighbours: %p -> %p\n", cur, NEXT_BLKP(cur));
+            exit(-1);
+        }
+        if (GET_ALLOC(HDRP(cur)) == 0) {
+            printf("free block: %p~%p\n", HDRP(cur), FTRP(cur) + WSIZE);
+            free_num_heap++;
+        }
+        cur = NEXT_BLKP(cur);
+    }
+
+    if (cur-1 != mem_heap_hi()) {
+        printf("Bad heap end: %p, should be: %p\n", cur-1, mem_heap_hi());
+        exit(-1);
+    }
+    if (free_num_heap != free_num_linked) {
+        printf("Free block numbers not match: %d!=%d\n", free_num_heap, free_num_linked);
+        exit(-1);
+    }
 }
 
 /*
@@ -299,7 +375,7 @@ void* place(void* bp, size_t asize) {
             coalesce(NEXT_BLKP(bp));
             return bp;
 
-        /* 分割后的块大小大于等于256，将分割后的块放在前面 */
+            /* 分割后的块大小大于等于256，将分割后的块放在前面 */
         } else {
             PUT(HDRP(bp), PACK(rest_size, 0));
             PUT(FTRP(bp), PACK(rest_size, 0));
@@ -309,13 +385,15 @@ void* place(void* bp, size_t asize) {
             return NEXT_BLKP(bp);
         }
 
-    /* 不可分割 */
-    } else {                
+        /* 不可分割 */
+    } else {
         PUT(HDRP(bp), PACK(block_size, 1));
         PUT(FTRP(bp), PACK(block_size, 1));
         return bp;
     }
 }
+
+/* 从小到大遍历空闲链表，找best-fit */
 void* find_fit(size_t asize) {
     /* 从小到大遍历空闲链表 */
     for (int i = search(asize); i < CLASS_NUM; i++) {
@@ -323,7 +401,7 @@ void* find_fit(size_t asize) {
         printf("find_fit: %d\n", i);
 #endif
         char* cur = GET_LINK_FIRST(i);
-        while (cur != heap_listp) {
+        while (cur != heap_start) {
             /* best fit，因为每个链表内维护大小有序 */
             if (GET_SIZE(HDRP(cur)) >= asize) {
                 return cur;
@@ -412,23 +490,23 @@ int search(size_t asize) {
 }
 
 
- /* 将位于bp的空闲块插入到对应大小类链表 */
+/* 将位于bp的空闲块插入到对应大小类链表 */
 void insert(void* bp) {
     size_t size = GET_SIZE(HDRP(bp));
     int index = search(size);
     char* first = GET_LINK_FIRST(index);
 
     /* 该空闲块插入到空链表 */
-    if (first == heap_listp) {
+    if (first == heap_start) {
         PUT(bp, NULL);
         PUT(bp + WSIZE, NULL);
-        PUT(heap_listp + index * WSIZE, bp);
+        PUT(heap_start + index * WSIZE, bp);
         return;
     }
 
     /* 该空闲块插入到链表中间 */
     char* cur = first;
-    while (GET_LINK_SUCC(cur) != heap_listp) {
+    while (GET_LINK_SUCC(cur) != heap_start) {
         char* next = GET_LINK_SUCC(cur);
         if (size <= GET_SIZE(HDRP(next))) {
             /* 该空闲块插入cur和next之间 */
@@ -449,42 +527,41 @@ void insert(void* bp) {
 
 /* 将位于bp的空闲块从对应大小类链表中删除 */
 void delete(void* bp) {
-    if (GET_LINK_PRED(bp) == heap_listp && GET_LINK_SUCC(bp) == heap_listp) {
-        PUT(heap_listp + search(GET_SIZE(HDRP(bp))) * WSIZE, NULL);
+    if (GET_LINK_PRED(bp) == heap_start && GET_LINK_SUCC(bp) == heap_start) {
+        PUT(heap_start + search(GET_SIZE(HDRP(bp))) * WSIZE, NULL);
         return;
     }
     /* 该空闲块位于链表中间 */
-    if (GET_LINK_PRED(bp) != heap_listp && GET_LINK_SUCC(bp) != heap_listp) {
+    if (GET_LINK_PRED(bp) != heap_start && GET_LINK_SUCC(bp) != heap_start) {
         PUT(GET_LINK_PRED(bp) + WSIZE, GET_LINK_SUCC(bp));
         PUT(GET_LINK_SUCC(bp), GET_LINK_PRED(bp));
         return;
     }
     /* 该空闲块位于链表头部 */
-    else if (GET_LINK_PRED(bp) == heap_listp && GET_LINK_SUCC(bp) != heap_listp) {
-        PUT(heap_listp + search(GET_SIZE(HDRP(bp))) * WSIZE, GET_LINK_SUCC(bp));
+    else if (GET_LINK_PRED(bp) == heap_start && GET_LINK_SUCC(bp) != heap_start) {
+        PUT(heap_start + search(GET_SIZE(HDRP(bp))) * WSIZE, GET_LINK_SUCC(bp));
         PUT(GET_LINK_SUCC(bp), NULL);
         return;
     }
     /* 该空闲块位于链表尾部 */
-    else if (GET_LINK_PRED(bp) != heap_listp && GET_LINK_SUCC(bp) == heap_listp) {
+    else if (GET_LINK_PRED(bp) != heap_start && GET_LINK_SUCC(bp) == heap_start) {
         PUT(GET_LINK_PRED(bp) + WSIZE, NULL);
         PUT(GET_LINK_SUCC(bp), NULL);
     }
     /* 该空闲块是链表中唯一一个 */
     else {
-        PUT(heap_listp + search(GET_SIZE(HDRP(bp))) * WSIZE, NULL);
+        PUT(heap_start + search(GET_SIZE(HDRP(bp))) * WSIZE, NULL);
     }
 }
-
 /* For debug */
 static void print_free_list() {
     printf("-------------------------------print_free_list-------------------------------\n");
     for (int i = 0; i < CLASS_NUM; i++) {
-        if (GET_LINK_FIRST(i) == heap_listp)
+        if (GET_LINK_FIRST(i) == heap_start)
             continue;
         printf("class %d: \n", i);
         char* cur = GET_LINK_FIRST(i);
-        while (cur != heap_listp) {
+        while (cur != heap_start) {
             printf("  0x%p~0x%p: %d=%d, pre:%p, suc:%p\n", HDRP(cur), FTRP(cur) + WSIZE, GET_SIZE(HDRP(cur)), GET_SIZE(FTRP(cur)), GET_LINK_PRED(cur), GET_LINK_SUCC(cur));
             cur = GET_LINK_SUCC(cur);
         }
